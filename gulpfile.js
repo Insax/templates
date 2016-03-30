26cfb4b516a18c2801d9a75bd2a22c925c55eb57
Insax <m.kram@live.at>
2016-03-30 21:45:43 +0200

    Fix Template

--- gulpfile.js
+++ gulpfile.js
@@ -13,17 +13,50 @@ var elixir = require('laravel-elixir');
 
 elixir(function(mix) {
     mix.sass('app.scss')
+       .sass('bootstrap.scss')
+       .sass('font-awesome.scss')
        .sass('magnific-popup.scss')
        .sass('owl.carousel.scss')
-       .sass('slider.scss')
+       .sass('settings.scss')
        .sass('sweet-alert.scss')
-       .browserify('smooth-scroll.js')
+       .sass('youplay.scss')
+       .sass('youplay-rtl.scss')
+       .browserify('bootstrap.js')
+       .browserify('jquery.hexagonprogress.js')
+       .browserify('jquery.js')
+       .browserify('shapes-polyfill.js')
+       .browserify('smoothscroll.js')
+       .browserify('sweet-alert.js')
+       .browserify('validator.js')
+       .browserify('youplay.js')
        .version([
             'css/app.css',
+            'css/bootstrap.css',
+            'css/font-awesome.css',
             'css/magnific-popup.css',
-            'css/owl.carousel.css',
-            'css/slider.css',
+            'css/settings.css',
             'css/sweet-alert.css',
-            'js/smooth-scroll.js'
+            'css/social-likes.min.css',
+            'css/youplay.css',
+            'css/youplay-rtl.css',
+            'js/bootstrap.js',
+            'js/html5shiv.min.js',
+            'js/isotope.pkgd.min.js',
+            'js/jarallax-video.js',
+            'js/jarallax.js',
+            'js/jquery.countdown.min.js',
+            'js/jquery.hexagonprogress.js',
+            'js/jquery.js',
+            'js/jquery.magnific-popup.min.js',
+            'js/jquery.themepunch.revolution.min.js',
+            'js/jquery.themepunch.tools.min.js',
+            'js/owl.carousel.min.js',
+            'js/shapes-polyfill.js',
+            'js/skrollr.min.js',
+            'js/smoothscroll.js',
+            'js/social-likes.min.js',
+            'js/sweet-alert.js',
+            'js/validator.js',
+            'js/youplay.js'
             ]);
 });
